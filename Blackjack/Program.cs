3b224dfd6787be7949b10777019b1399c7621aa6
Nicholas Page <npage@southhills.edu>
2023-11-29 17:30:27 -0500

    Initial commit

---
+++ Blackjack/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Name: [YOUR NAME HERE]
+ * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+ */
+
+namespace Blackjack
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
